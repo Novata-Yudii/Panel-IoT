a70150d82e1f065ece38acf704889784ee9a8dd7
Novata-Yudii <novatawahyudi@gmail.com>
2024-05-15 15:11:50 +0700

    Modifikasi refactoring kode

--- resources/views/pages/temperature-mqtt.blade.php
+++ resources/views/pages/temperature-mqtt.blade.php
@@ -11,9 +11,7 @@
 <script>
 let chart;
 async function requestData(msg) {
-    const temp = msg.temperature;
-    console.log(temp);
-    // const time = data.data[0].created_at;
+    let temp = (msg.temperature).toFixed(3);
     const point = [new Date().getTime(), parseFloat(temp)];
     const series = chart.series[0], shift = series.data.length > 20;
     chart.series[0].addPoint(point, true, shift);
@@ -54,19 +52,16 @@ window.addEventListener('load', function () {
         }
         ]
     });
-})
-</script>
 
-<script>
-    window.addEventListener('load', function () {
-        const url = 'wss://x10ea311.ala.asia-southeast1.emqxsl.com:8084/mqtt'
-        const options = {
-            clean: true,
-            connectTimeout: 4000,
-            clientId: 'emqx-novatayudi',
-            username: 'Novatayudi',
-            password: 'Xos116ya77',
-            ca:`-----BEGIN CERTIFICATE-----
+    // Konfigurasi connect to mqtt broker emqx OVER MQTTS
+    const url = 'wss://x10ea311.ala.asia-southeast1.emqxsl.com:8084/mqtt'
+    const options = {
+        clean: true,
+        connectTimeout: 4000,
+        clientId: 'emqx-novatayudi',
+        username: 'Novatayudi',
+        password: 'Xos116ya77',
+        ca:`-----BEGIN CERTIFICATE-----
 MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
 MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
 d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
@@ -88,36 +83,36 @@ PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
 YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
 CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
 -----END CERTIFICATE-----`
+        }
+    const client  = mqtt.connect(url, options)
+    client.on('connect', function () {
+    console.log('Connected')
+        client.subscribe('/temperature', function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Subscribe in /temperature')
             }
-        const client  = mqtt.connect(url, options)
-        client.on('connect', function () {
-        console.log('Connected')
-            client.subscribe('/temperature', function (err) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log('Subscribe in /temperature')
-                }
-            })
-            let pub = {value : 1}
-            pub = JSON.stringify(pub);
-            client.publish('/lampu', pub, function(err){
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log('Published')
-                }
-            })
         })
-
-        client.on('message', async function (topic, message) {
-            if(topic == '/temperature'){
-                const msg = JSON.parse(message)
-                if(typeof msg == 'object'){
-                    await requestData(msg)
-                }
+        let pub = {value : 0}
+        pub = JSON.stringify(pub);
+        client.publish('/lampu', pub, function(err){
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Published')
             }
         })
     })
+
+    client.on('message', async function (topic, message) {
+        if(topic == '/temperature'){
+            const msg = JSON.parse(message)
+            if(typeof msg == 'object'){
+                await requestData(msg)
+            }
+        }
+    })
+})  
 </script>
 @endpush
